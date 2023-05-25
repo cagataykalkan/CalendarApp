048f678aa5bfece8397efa42677fe934d098e5b8
Çağatay Kalkan <c.kalkan203@gmail.com>
2023-05-25 20:21:26 +0300

    admin ve takvim classları eklendi
    admin ve takvim classları ve fonksiyonları eklendi

--- CalendarApp.py
+++ CalendarApp.py
@@ -20,3 +20,54 @@ class Kullanıcı:
         self.telefon_no = telefon_no
         self.mail_adresi = mail_adresi
         self.adres = adres
+
+#Admin Kullanıcı Class'ı ve Fonksiyonları
+class AdminKullanıcı(Kullanıcı):
+    def __init__(self,ad,soyad,kullanıcıAdı,parola,tc_no,telefon_no,mail_adresi,adres):
+        super().__init__(ad,soyad,kullanıcıAdı,parola,tc_no,telefon_no,mail_adresi,adres)
+
+    def takvimi_görüntüle(self, takvim):
+        print("\n----- Tüm Olaylar -----")
+        for olay in takvim.olaylar:
+            print(f"Sahibi: {olay.sahibi.kullanıcıAdı}")  #Olay Sahibinin Kullanıcı Adını Gösterir
+            print(f"Tarih: {olay.tarih}")
+            print(f"Başlık: {olay.başlık}")
+            print(f"Açıklama: {olay.açıklama}")
+            print("-----------------------")
+
+#Takvim Class'ı ve Fonksiyonları
+class Takvim:
+    def __init__(self):
+        self.olaylar = []
+
+    def olay_ekle(self, olay):
+        self.olaylar.append(olay)
+        print("|||Olay Başarıyla Eklendi.|||")
+
+    def tarihe_göre_olay_ekle(self, tarih):
+        tarihteki_olaylar = []
+        for olay in self.olaylar:
+            if olay.tarih == tarih:
+                tarihteki_olaylar.append(olay)
+        return tarihteki_olaylar
+
+    def olay_silme(self, olay):
+        if olay in self.olaylar:
+            self.olaylar.remove(olay)
+            print("|||Olay Başarıyla Silindi.|||")
+        else:
+            print("|||Olay Bulunamadı.|||")
+
+    def olay_güncelle(self, olay, yeni_başlık, yeni_açıklama):
+        if olay in self.olaylar:
+            olay.başlık = yeni_başlık
+            olay.açıklama = yeni_açıklama
+            print("|||Olay Başarıyla Güncellendi.|||")
+        else:
+            print("|||Olay Bulunamadı.|||")
+
+# Kullanıcı ve admin kayıtları
+kullanıcılar = []
+
+# Takvim oluşturma
+takvim = Takvim()
